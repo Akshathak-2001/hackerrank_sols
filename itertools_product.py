56cdd8e677df369b90d252d830782ebd3c7af847
Akshathak-2001 <82942249+Akshathak-2001@users.noreply.github.com>
2023-11-22 21:54:07 +0530

    Add files via upload

---
+++ itertools_product.py
@@ -0,0 +1,9 @@
+'''You are given a two lists A and B. Your task is to compute their cartesian product AXB.
+'''
+
+from itertools import product
+
+A = list(map(int, input().split()))
+B = list(map(int,input().split()))
+res = list(product(A,B))
+print(*res)
